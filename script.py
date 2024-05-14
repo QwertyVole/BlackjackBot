e07f5fbc395af5cc0fb85b27eed0d2d30924bf85
Karel Kalina <karel.kalina@marketup.cz>
2024-05-14 13:16:22 +0200

    RTMP stream barebones

--- script.py
+++ script.py
@@ -1,5 +1,7 @@
 import cv2 as cv
 
-img = cv.imread("Pictures/OldVan.jpg")
-cv.imshow("Cat", img)
-cv.waitKey(0)
+#get rtmp stream to work
+
+myrtmp_addr = "rtmp://myip:1935/myapp/mystream"
+cap = cv.VideoCapture(myrtmp_addr)
+frame,err = cap.read()
