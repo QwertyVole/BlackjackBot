86969c830136210f54d40b788efb6574ebc981d0
Karel Kalina <karel.kalina@marketup.cz>
2024-05-28 21:10:11 +0200

    ImgRecognition working: tesseract and upkeep

--- ImgRecognition.py
+++ ImgRecognition.py
@@ -1,17 +1,23 @@
 import cv2 as cv
 import numpy as numpy
-import pytesseract as pytes
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
 
-#cap = cv.VideoCapture(0)
-##if 0 doesnt work, increment by one and try again
-# while True:
-#     isTrue, frame = cap.read()
-#     cv.imshow("Video", frame)
-# #press d to destroy all windows and stop the program
-#     if cv. waitKey(20) & 0xFF==ord("d"):
-#         break
-# cap.release()
-# cv.destroyAllWindows()
-img =cv.imread("BJBOT/png2.png")
-cv.imshow("img", img)
+custom_config = r'--oem 3 --psm 6' 
+
+
+cap = cv.VideoCapture(0)
+#if 0 doesnt work, increment by one and try again
+while True:
+    isTrue, img = cap.read()
+    cv.imshow("Video", img)
+    img = img[596:612, 618:636 ]        
+    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    print(pytesseract.image_to_string(img, config = custom_config))
+#press d to destroy all windows and stop the program
+    if cv. waitKey(20) & 0xFF==ord("d"):
+        break
+
 cv. waitKey(0)
+cap.release()
+cv.destroyAllWindows()
