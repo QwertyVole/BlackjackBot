a64fa90dec894ad336da14294a6695bcacd75c29
Karel Kalina <karel.kalina@marketup.cz>
2024-06-18 00:16:17 +0200

    neplecha ukoncena

---
+++ README.txt
@@ -0,0 +1,16 @@
+Ahoj, doufame, ze jsi nejdrive otevrel tenhle readme file nez jsi zacal prochazet nasimi soubory.
+
+DULEZITE!!
+	- funkcni script je rozdelen do dvou sobouru(puvodne byl v jednom ale momentalne je ve dvou, udelal to Karel) a to: main.py, utils.py
+	- uvidis je vlastne hned po otevreni repozitare
+	- slozka Archive zobrazuje nasi vsechnu snahu, krev, pot, sily co jsme do toho dali
+		tim je mysleno pocet pokusovych souboru, mini-cilu, ruznych postupu, ktere jsme se snazili zabudovat do finalni stage naseho projektu
+		bohuzel hodne z toho se zdalo jednoduche, ale pri samotnem implementovani jsme zjistili, ze nase moznosti nejsou na takove urovni..
+		s tim spojene je moralka...po tom co ti 3h vkuse nejde spustit kod a obesel jsi vsechen stackoverflow, chatgpt ti zase s prominutim vyblil sr***ku?
+		tak proste si clovek rekne, ze na to nema, nebo ze na to proste sere a radeji se bude venovat dulezitejsim aspektum patrici ke kostre projektu nez k nejakym 'features',
+		take tam najdes ne malo screenshotu pro zkouseni/testovani/learning bota
+	- obsahuje to take, snad si myslime, finalni prezentaci, kde je shrnuto to ceho jsme dosahli/nedosahli, celkove hodnoceni + DULEZITE! DEMO fungovani celeho naseho scriptu(projektu)
+
+	- we are proud of what we achieved and completely disgusted by the reality
+
+	https://docs.google.com/presentation/d/1n9QnmY9yrL426L_RLbY5rwEVwZuE_HfntkiyfCtrJ7Q/edit?usp=sharing
--- main.py
+++ main.py
@@ -2,55 +2,58 @@ import utils as u
 
 def main():
     round_in_progress = False
+    previous_message = ""
     while True:
         # Capture the screen
         screenshot = u.capture_screen()
         if not round_in_progress and u.check_message(screenshot, u.place_bets_coords, "PLACE YOUR BETS"):
             # Check if the start of the round is triggered by detecting "PLACE YOUR BETS"
+            previous_message = "PLACE YOUR BETS"
             print("Start of a new round detected")
             u.click_position(u.betting_unit_coords)
             u.click_position(u.betting_position_coords)
             round_in_progress = True
-        else:
+        elif u.check_message(screenshot, u.make_decision_coords, "DEALING"):
+            previous_message = "DEALING"
+        elif u.check_message(screenshot, u.make_decision_coords, "MAKE YOUR DECISION"):
             # Check if the round is underway by detecting "MAKE YOUR DECISION"
-            if u.check_message(screenshot, u.make_decision_coords, "MAKE YOUR DECISION"):
-                print("Round underway, making decisions")
-                # Extract player total area
-                player_total_area = u.extract_area(screenshot, u.player_total_coords)
-                player_total_text = u.perform_ocr(player_total_area)
-                player_total_text = u.filter_card_value(player_total_text)
-                #print(f"Player total text: {player_total_text}")
+            print("Round underway, making decisions")
+            # Extract player total area
+            player_total_area = u.extract_area(screenshot, u.player_total_coords)
+            player_total_text = u.perform_ocr(player_total_area)
+            player_total_text = u.filter_card_value(player_total_text)
+            #print(f"Player total text: {player_total_text}")
 
-                # Extract dealer total area
-                dealer_total_area = u.extract_area(screenshot, u.dealer_total_coords)
-                dealer_total_text = u.perform_ocr(dealer_total_area)
-                dealer_total_text = u.filter_card_value(dealer_total_text)
-                #print(f"Dealer total text: {dealer_total_text}")
+            # Extract dealer total area
+            dealer_total_area = u.extract_area(screenshot, u.dealer_total_coords)
+            dealer_total_text = u.perform_ocr(dealer_total_area)
+            dealer_total_text = u.filter_card_value(dealer_total_text)
+            #print(f"Dealer total text: {dealer_total_text}")
 
-                # Convert totals to values
-                player_total_value = u.card_value(player_total_text)
-                dealer_card_value = u.card_value(dealer_total_text)
+            # Convert totals to values
+            player_total_value = u.card_value(player_total_text)
+            dealer_card_value = u.card_value(dealer_total_text)
 
-                print(f"Player total value: {player_total_value}")
-                print(f"Dealer card value: {dealer_card_value}")
+            print(f"Player total value: {player_total_value}")
+            print(f"Dealer card value: {dealer_card_value}")
+               
+            if player_total_value != 0 and dealer_card_value != 0 and dealer_card_value < 17:
+                action = u.decide_action(player_total_value, dealer_card_value)
+                print(f"Player Total: {player_total_value}")
+                print(f"Dealer Total: {dealer_card_value}")        
+                print(f"Decision: {action}")
 
-                if player_total_value != 0 and dealer_card_value != 0 and dealer_card_value < 17:
-                    action = u.decide_action(player_total_value, dealer_card_value)
-                    print(f"Player Total: {player_total_value}")
-                    print(f"Dealer Total: {dealer_card_value}")
-                        
-                    print(f"Decision: {action}")
-
-                    # Perform the action based on the decision immediately after printing the decision
-                    if action == 'Hit':
-                        u.triple_click_position(u.hit_button_coords)
-                    elif action == 'Stand':
-                        u.triple_click_position(u.stand_button_coords)
-                    elif action == 'Double':
-                        u.triple_click_position(u.double_button_coords)
-            elif u.check_message(screenshot, u.place_bets_coords, "PLACE YOUR BETS"):
-                print("End of round")
-                break    
+                # Perform the action based on the decision immediately after printing the decision
+                if action == 'Hit':
+                    u.triple_click_position(u.hit_button_coords)
+                elif action == 'Stand':
+                    u.triple_click_position(u.stand_button_coords)
+                elif action == 'Double':
+                    u.triple_click_position(u.double_button_coords)
+        if round_in_progress == True and previous_message != "PLACE YOUR BETS" and u.check_message(screenshot, u.place_bets_coords, "PLACE YOUR BETS"):
+            print("End of round")
+            break
+               
 
 main()                            
 if __name__ == "__main__":
